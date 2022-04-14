a6d14d713af54eb340837789adeb323a854aaba1
ddenis1994 <ddenis1994@gmail.com>
2022-04-14 09:37:24 +0300

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Header from "./features/header/Header";
 
 function App() {
   return (
-    <div className="p-2 flex flex-col gap-2 md:h-auto lg:h-screen w-screen dark:text-white dark:bg-slate-700 ">
+    <div className="p-2 flex flex-col gap-2 md:h-auto lg:h-screen  dark:text-white dark:bg-slate-700 ">
       <Header />
       <div className={`border w-full`} />
 
